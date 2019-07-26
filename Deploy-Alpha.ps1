4a95feafd066f026781a4b996e3e4c047a68ac95
Tim Long <Tim@tigranetworks.co.uk>
2019-07-26 23:37:02 +0100

    Build support changes

--- Deploy-Alpha.ps1
+++
@@ -1,2 +0,0 @@
-Copy-Item .\TA.NexDome.Rotator\Release\TA.NexDome.Rotator.hex -Destination 'F:\OneDrive\Clients\NexDome (Babak Sedehi)\NexDome Alpha\Latest\' -Force
-Copy-Item .\TA.NexDome.Shutter\Release\TA.NexDome.Shutter.hex -Destination 'F:\OneDrive\Clients\NexDome (Babak Sedehi)\NexDome Alpha\Latest\' -Force
